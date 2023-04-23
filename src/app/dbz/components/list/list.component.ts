2aeaff22cd9ff13acea8257a236062d65c320039
Daniel Tirado <jesus.tirado@vitalbox.co>
2023-04-23 14:53:57 -0500

    forms y inputs y outputs

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: "Trunks",
+      power: 10
+    }
+  ];
+
+  @Output()
+  public onDeleteIndex: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number): void {
+    console.log(index);
+    this.onDeleteIndex.emit(index);
+  }
+}
